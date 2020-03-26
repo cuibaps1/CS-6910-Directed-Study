516727f66f9ec6b54f5f9bb2a7cbc83e826033b9
cuibaps1 <jadanngo@gmail.com>
2020-03-26 12:31:26 -0700

    finished timer

---
+++ Master Project/Assets/Scripts/LapComplete.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapComplete : MonoBehaviour {
+
+    public GameObject LapCompleteTrig;
+    public GameObject HalfLapTrig;
+
+    public GameObject MinuteDisplay;
+    public GameObject SecondDisplay;
+    public GameObject MilliDisplay;
+
+    public GameObject LapTimeBox;
+
+    void OnTriggerEnter()
+    {
+        if (TimeManager.SecondCount <= 9)
+        {
+            SecondDisplay.GetComponent<Text>().text = "0" + TimeManager.SecondCount + ".";
+        }
+        else
+        {
+            SecondDisplay.GetComponent<Text>().text = "" + TimeManager.SecondCount + ".";
+        }
+
+        if(TimeManager.MinuteCount <= 9)
+        {
+            MinuteDisplay.GetComponent<Text>().text = "0" + TimeManager.MinuteCount + ".";
+        }
+        else
+        {
+            MinuteDisplay.GetComponent<Text>().text = "" + TimeManager.MinuteCount + ".";
+        }
+
+        MilliDisplay.GetComponent<Text>().text = "" + TimeManager.MilliCount;
+        TimeManager.MinuteCount = 0;
+        TimeManager.SecondCount = 0;
+        TimeManager.MilliCount = 0;
+
+        HalfLapTrig.SetActive(true);
+        LapCompleteTrig.SetActive(false);
+    }
+}
